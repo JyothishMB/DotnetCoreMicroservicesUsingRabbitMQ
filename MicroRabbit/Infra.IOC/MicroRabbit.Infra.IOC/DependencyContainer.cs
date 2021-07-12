d837257d0ed330592bd64040877ca7158e1aba72
Jyothish <jyothishmb1992@gmail.com>
2021-07-12 23:23:42 +0530

    Initial Commit for IOC Project.

---
+++ MicroRabbit/Infra.IOC/MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            
+        }
+    }
+}
